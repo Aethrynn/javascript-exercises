6c3d4b60e590747bd033a3a707cd7e2507e2957f
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-28 16:54:14 -0500

    completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, numOfRepeats) {
 
+    let repeatedString = '';
+
+    if(numOfRepeats >= 0) {
+        for (let i = 0; i < numOfRepeats; i++) {
+            repeatedString = repeatedString + text;
+        }
+    } else {
+        return "ERROR";
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line

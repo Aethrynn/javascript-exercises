24665a259c1ca79d1735f86c445ee1f7c6d18ce5
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-28 17:10:16 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...valuesToRemove) {
+    let updatedArray = [];
+
+    for(const value of originalArray){
+        if (!valuesToRemove.includes(value)) {
+            updatedArray.push(value);
+        }
+    }
+
+    return updatedArray;
 };
 
 // Do not edit below this line

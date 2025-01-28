1c22b7503759b56b211d0da42a9ba934e481ea3d
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-28 16:57:50 -0500

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let reversed = '';
 
+    for(let i = stringToReverse.length; i >= 0; i--){
+        reversed = reversed + stringToReverse.charAt(i);
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line

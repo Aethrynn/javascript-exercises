fe7cecad8da9eaa09701c02f1afe9e018201e1f8
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-29 16:58:14 -0500

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+
+  let convertedTemp = ((fahrenheitTemp - 32) * (5/9));
+
+  if(!Number.isInteger(convertedTemp)) {
+    convertedTemp = parseFloat(convertedTemp.toFixed(1));
+  }
+
+  return convertedTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  
+  let convertedTemp = ((celsiusTemp * 9/5) + 32);
+
+  if(!Number.isInteger(convertedTemp)) {
+    convertedTemp = parseFloat(convertedTemp.toFixed(1));
+  }
+
+  return convertedTemp
 };
 
 // Do not edit below this line
